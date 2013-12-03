8c434666b3b47347a56a2c550e6482177c0f2e3e
Emily Lu Wang <emilyluwang@Emilys-MacBook-Air-2.local>
2013-12-03 17:49:19 -0500

    adding some backend php stuff

---
+++ public/controllers/constants.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Adapted from Computer Science 50
+     * Problem Set 7
+     *
+     * Global constants.
+     */
+
+    // our database's name
+    define("DATABASE", "pset7");
+
+    // our database's password
+    define("PASSWORD", "leclado");
+
+    // our database's server
+    define("SERVER", "localhost");
+
+    // our database's username
+    define("USERNAME", "leclado");
+
+?>
